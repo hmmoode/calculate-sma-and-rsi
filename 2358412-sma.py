d101d3dbf6b058441f03fc6af3f76a4ee95c3e76
hmmoode <155687101+hmmoode@users.noreply.github.com>
2024-01-07 21:50:03 +0300

    Delete 2358412-sma.py

--- 2358412-sma.py
+++
@@ -1,9 +0,0 @@
-def calculate_sma(data, window=5):
-    sma = []
-    for i in range(len(data)):
-        if i < window:
-            sma.append(None)
-        else:
-            sum_close = sum(float(row['Close']) for row in data[i - window:i])
-            sma.append(sum_close / window)
-    return sma
